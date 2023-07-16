3c260dc7cdd7dcf878063676193b4f53740ef468
Chris Otey <cotey-cw@outlook.com>
2023-07-16 18:20:26 -0400

    wrap app in ServicesProvider

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import SignInForm from './components/sign-in';
+import { ServicesProvider } from './context/ServicesProvider';
 import { AuthenticationProvider } from './context'
 
 function App() {
   return (
     <div className="App">
-      <AuthenticationProvider>
-        <SignInForm />
-      </AuthenticationProvider>
+      <ServicesProvider>
+        <AuthenticationProvider>
+          <SignInForm />
+        </AuthenticationProvider>
+      </ServicesProvider>
     </div>
   );
 }
