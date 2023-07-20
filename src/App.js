7ba8a5773e095d7e55de22e05ecc6ec03300b9fa
dking124 <60586385+dking124@users.noreply.github.com>
2023-07-20 00:01:59 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import "./App.css";
 import SignInForm from "./components/sign-in";
+import Tasks from "./components/tasks";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ServicesProvider } from "./context/ServicesProvider";
 import { AuthenticationProvider } from "./context";
 
 function App() {
   return (
     <div className="App">
-      <ServicesProvider>
-        <AuthenticationProvider>
-          <SignInForm />
+      <BrowserRouter>
+        <ServicesProvider>
+          <AuthenticationProvider>
+            <Routes>
+              <Route path="/" element={<SignInForm />}/>
+              <Route path="/sign_in" element={<SignInForm />}/>
+              <Route path="/tasks" element={<Tasks />}/>
+            </Routes>
         </AuthenticationProvider>
       </ServicesProvider>
+      </BrowserRouter>
     </div>
   );
 }
