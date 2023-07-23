63e08bd70ad5477526ce395d17263e1d62b2aae2
ConnorDBurge <connor.d.burge@gmail.com>
2023-07-23 15:12:15 -0400

    changed back to test

--- src/services/HttpService.js
+++ src/services/HttpService.js
@@ -4,7 +4,7 @@ const local = "http://localhost:8080/v1";
 const test = "https://task-management-api-test-5ef9ebfe6fb3.herokuapp.com/v1";
 
 const http = axios.create({
-  baseURL: local,
+  baseURL: test,
   headers: {
     "Content-Type": "application/json",
   },
