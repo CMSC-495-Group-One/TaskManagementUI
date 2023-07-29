d590f81bd65b8519ca901bc34e4dfb4594733126
Hasib Rostaiee <60456566+hrostaiee@users.noreply.github.com>
2023-07-29 13:07:37 -0400

    Changes to InputField.js to include type and InputProps Changes to sign-in.js for changing UI to MUI elements.

--- src/components/sign-in.js
+++ src/components/sign-in.js
@@ -1,59 +1,178 @@
-import React, { useState } from "react";
-import { useForm, FormProvider } from "react-hook-form";
-import { useAuth } from "../context/AuthProvider";
-import { useNavigate } from "react-router-dom";
+import React, {useState} from "react";
+import {useForm, FormProvider} from "react-hook-form";
+import {useAuth} from "../context/AuthProvider";
+import {useNavigate} from "react-router-dom";
+import {makeStyles} from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Avatar from "@material-ui/core/Avatar";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import InputField from "./InputField";
+import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
+import Link from "@material-ui/core/Link";
+import Button from "@material-ui/core/Button";
+import {IconButton, InputAdornment} from "@material-ui/core";
+import {Visibility, VisibilityOff} from "@material-ui/icons";
+import TextField from "@material-ui/core/TextField";
 
-const SignInForm = () => {
-  const formMethods = useForm();
-  const { signIn } = useAuth();
-  const navigate = useNavigate();
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = formMethods;
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%',
+        marginTop: theme.spacing(3),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
 
-  const [isLoading, setIsLoading] = useState(false);
-  const [loginError, setLoginError] = useState(null);
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://github.com/CMSC-495-Group-One">
+                {/* Your Website */}
+                Git Repo
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
 
-  const onSubmit = async (data) => {
-    setIsLoading(true);
-    setLoginError(null);
-    try {
-      await signIn(data);
-      setIsLoading(false);
-      navigate("/tasks");
-    } catch (error) {
-      setIsLoading(false);
-      // handle error, e.g. show a message to the user
-      setLoginError("Invalid username or password.");
+export default function SignInForm() {
+    const classes = useStyles();
+    //const { handleSubmit, control, formState: { errors } } = useForm();
+
+    const formMethods = useForm();
+    const {signIn} = useAuth();
+    const navigate = useNavigate();
+
+    const {
+        register,
+        control,
+        handleSubmit,
+        formState: {errors},
+    } = formMethods;
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [loginError, setLoginError] = useState(null);
+
+    const onSubmit = async (data) => {
+        setIsLoading(true);
+        setLoginError(null);
+        try {
+            await signIn(data);
+            setIsLoading(false);
+            navigate("/tasks");
+        } catch (error) {
+            setIsLoading(false);
+            // handle error, e.g. show a message to the user
+            setLoginError("Invalid username or password.");
+        }
     }
-  };
+    const [showPassword, setShowPassword] = useState(false);
 
-  return (
-    <FormProvider {...formMethods}>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <label>Username:</label>
-          <input
-            {...register("username", { required: true })}
-            placeholder="Username"
-          />
-        </div>
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
 
-        <div>
-          <label>Password:</label>
-          <input
-            {...register("password", { required: true })}
-            placeholder="Password"
-            type="password"
-          />
-        </div>
-        <input type="submit" />
-      </form>
-    </FormProvider>
-  );
-};
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
 
-export default SignInForm;
+
+    return (
+        <Container component="main" maxWidth="xs">
+            <CssBaseline/>
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon/>
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign In
+                </Typography>
+                <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <InputField
+                                name="username"
+                                control={control}
+                                defaultValue=""
+                                rules={{
+                                    required: 'Username is required.',
+                                }}
+                                autoComplete="email"
+                                label="Username"
+                                error={!!errors.email}
+                                helperText={errors.email?.message}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                name="password"
+                                label="Password"
+                                control={control}
+                                defaultValue=""
+                                type={showPassword ? "text" : "password"}
+                                required
+                                fullWidth
+                                variant="outlined"
+                                InputProps={
+                                {
+                                    endAdornment: (
+                                        <InputAdornment position="end">
+                                        <IconButton
+                                            aria-label="toggle password visibility"
+                                            onClick={handleClickShowPassword}
+                                            onMouseDown={handleMouseDownPassword}
+                                            edge="end"
+                                                    >
+                                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                                        </IconButton>
+                                        </InputAdornment>
+                                    )
+
+                                            }}
+                                rules={{
+                                    required: 'Password is required',
+                                }}
+                                error={!!errors.password}
+                                helperText={errors.password?.message}
+                            />
+                        </Grid>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                        >
+                            Sign In
+                        </Button>
+                        <Grid container justifyContent="flex-end">
+                            <Grid item>
+                                <Link href="/sign-up" variant="body2">
+                                    Don't have an account? Sign Up
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </form>
+            </div>
+            <Box mt={5}>
+                <Copyright/>
+            </Box>
+        </Container>
+    );
+}
