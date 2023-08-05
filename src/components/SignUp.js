59cd6c98e9bccd98cdf94c11007962a37ac2054a
Hasib Rostaiee <60456566+hrostaiee@users.noreply.github.com>
2023-08-05 15:07:54 -0400

    Fixed typo for sign in page href in sign up

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -222,7 +222,7 @@ export default function SignUp() {
                     </Button>
                     <Grid container justifyContent="flex-end">
                         <Grid item>
-                            <Link href="/SignIn" variant="body2">
+                            <Link href="/sign-in" variant="body2">
                                 Already have an account? Sign in
                             </Link>
                         </Grid>
